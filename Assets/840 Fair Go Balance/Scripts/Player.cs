05181a5dd65a88417ee352b922a18c1b0fddb1d9
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-12-10 14:30:15 +0300

    update

--- Assets/840 Fair Go Balance/Scripts/Player.cs
+++ Assets/840 Fair Go Balance/Scripts/Player.cs
@@ -2,8 +2,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Camera _camera;
+    private float nextTime;
+    private const float rate = 0.1f;
 
-    private void Awake() => _camera = Camera.main;
-    private void Start() => transform.position = new Vector2(0, 1.25f);
+    private Vector2 touchDirection;
+
+    private Camera Camera { get; set; }
+    private Rigidbody2D Rigidbody { get; set; }
+
+    private void Awake()
+    {
+        Camera = Camera.main;
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        transform.position = new Vector2(0, 1.25f);
+    }
+
+    private void Update()
+    {
+        if(Time.time > nextTime)
+        {
+            nextTime = Time.time + rate;
+            touchDirection = new Vector2(Random.Range(-1.0f, 1.0f), 0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody.AddForce(touchDirection);
+    }
 }
