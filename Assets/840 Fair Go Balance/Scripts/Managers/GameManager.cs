11e0a5470228863bd3eb633171ba090c1e70c0cb
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-12-10 14:04:12 +0300

    init

--- Assets/840 Fair Go Balance/Scripts/Managers/GameManager.cs
+++ Assets/840 Fair Go Balance/Scripts/Managers/GameManager.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System;
-using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,12 +8,10 @@ public class GameManager : MonoBehaviour
     private bool GameStarted { get; set; }
     private float GameSpeed { get; set; } = 0.5f;
     public float Score { get; set; }
+
+
     private Player Player { get; set; }
-
-
-    private Enemy Enemy { get; set; }
-    private Transform EnemyParent { get; set; }
-
+    private Transform EnvironmentRef { get; set; }
 
     public static Action OnHandlePulled { get; set; } = delegate { };
     public static Action<float> OnGameFinsihed { get; set; } = delegate { };
@@ -22,9 +19,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Player = Resources.Load<Player>("player");
-
-        Enemy = Resources.Load<Enemy>("enemy");
-        EnemyParent = GameObject.Find("Environment").transform;
+        EnvironmentRef = GameObject.Find("Environment").transform;
     }
 
     private void Update()
@@ -41,50 +36,13 @@ public class GameManager : MonoBehaviour
     {
         Score = 0;
 
-        Instantiate(Player, GameObject.Find("Environment").transform).transform.position = new Vector2(0, -3.47f);
-
-        StartCoroutine(nameof(Spawning));
+        Instantiate(Player, EnvironmentRef);
         GameStarted = true;
     }
 
     public void GameOver()
     {
         OnGameFinsihed?.Invoke(Score);
-        DeleteAllObstacles();
-
-        StopCoroutine(nameof(Spawning));
         GameStarted = false;
-    }
-
-    public void ExitToMenu()
-    {
-        Destroy(FindObjectOfType<Player>().gameObject);
-        DeleteAllObstacles();
-
-        StopCoroutine(nameof(Spawning));
-        GameStarted = false;
-    }
-
-    private void DeleteAllObstacles()
-    {
-        Enemy[] obstacles = FindObjectsOfType<Enemy>();
-        foreach (Enemy ob in obstacles)
-        {
-            Destroy(ob.gameObject);
-        }
-    }
-
-    private IEnumerator Spawning()
-    {
-        float delay = 3.5f;
-
-        while(true)
-        {
-            Vector2 position = new Vector2(UnityEngine.Random.Range(-1.83f, 1.83f), 7.0f);
-            Quaternion rotation = Quaternion.Euler(Vector3.forward * UnityEngine.Random.Range(0.0f, 360.0f));
-
-            Instantiate(Enemy, position, rotation, EnemyParent);
-            yield return new WaitForSeconds(delay);
-        }
     }
 }
